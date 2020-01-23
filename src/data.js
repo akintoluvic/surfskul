7b0f2a3cbb0d5c9ce6724993595d91298fa9fffc
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-23 18:59:05 +0100

    data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const events = [
+    {
+        city: 'Hawai',
+        date: 'January 2-22',
+        book: 'Booking closed'
+    },
+    {
+        city: 'Sri Lanka',
+        date: 'February 1-15',
+        book: 'Booking open'
+    },
+    {
+        city: 'Sri Lanka',
+        date: 'March 5-19',
+        book: 'Booking open'
+    }
+]
+
+export default events
