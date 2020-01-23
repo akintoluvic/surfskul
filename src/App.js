8e498881331237880ad032835dff0a239413b4e9
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-23 17:25:01 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='as' >
+        <a href='/'>
+          <img src={logo} alt='logo' />
+        </a>
+        <nav>
+          <a href='/'>About</a>
+          <a href='/'>Teachers</a>
+          <a href='/'>Price</a>
+          <a href='/'>Schedule</a>
+          <a href='/'>Contacts</a>
+        </nav>
+      </header>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
